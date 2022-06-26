cfdbf3015773a122f6f66e3d568d8e74404a3bb5
Benoît V <benoit@vandanjon.fr>
2022-06-26 18:30:43 +0200

    add DB connection

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,22 @@
 require("dotenv").config();
 
 const express = require("express");
+const connection = require("./db-config");
 
 const app = express();
+
 const port = parseInt(process.env.SERVER_PORT ?? "5000", 10);
 
 app.use((req, res) => {
 	res.json({ message: "Request read" });
+});
+
+connection.connect((err) => {
+	if (err) {
+		console.error("error connecting: " + err.stack);
+	} else {
+		console.log("connected as id " + connection.threadId);
+	}
 });
 
 app.listen(port, (err) => {
