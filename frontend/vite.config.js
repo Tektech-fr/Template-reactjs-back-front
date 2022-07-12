629ecb1bd8be15925853ee9864e1102267e815f7
Benoît V <benoit@vandanjon.fr>
2022-07-12 19:39:20 +0200

    add aliases path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const path = require("path");
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@Comp": path.resolve(__dirname, "src/components"),
+			"@Page": path.resolve(__dirname, "src/pages"),
+			"@SassC": path.resolve(__dirname, "src/styles/components"),
+			"@SassP": path.resolve(__dirname, "src/styles/pages"),
+		},
+	},
+});
+
