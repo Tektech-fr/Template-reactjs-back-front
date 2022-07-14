497869058222b8ddbcc5f5c46c34a5a1f5f7770d
Benoît V <benoit@vandanjon.fr>
2022-07-14 12:23:01 +0200

    add 404 page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "@page/Home";
 import Login from "@page/Login";
+import NotFound from "@page/NotFound";
 
 const App = () => {
 	return (
@@ -9,6 +10,7 @@ const App = () => {
 			<Routes>
 				<Route path="/home" element={<Home />} />
 				<Route path="/login" element={<Login />} />
+				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
 	);
