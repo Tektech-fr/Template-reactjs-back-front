dc2ba1ecfd55e79d81697e402ce033024c7626cb
Benoît V <benoit@vandanjon.fr>
2022-07-15 14:14:30 +0200

    add UserTable comp to Home

---
+++ frontend/src/components/UserTable.jsx
@@ -0,0 +1,23 @@
+const UserTable = () => (
+	<table>
+		<thead>
+			<tr>
+				<th>Name</th>
+				<th>Username</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>Name data</td>
+				<td>Username data</td>
+				<td>
+					<button>Edit</button>
+					<button>Delete</button>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+);
+
+export default UserTable;
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,16 @@
+import UserTable from "@comp/UserTable";
+
 const Home = () => {
 	return (
 		<main>
 			<h1>Home CRUD</h1>
-			<section>Add user</section>
-			<section>View users</section>
+			<section>
+				<h2>Add user</h2>
+			</section>
+			<section>
+				<h2>View users</h2>
+				<UserTable />
+			</section>
 		</main>
 	);
 };
