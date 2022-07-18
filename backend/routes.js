e480ada8391cd3bad4d466589f44bbf997992ba6
Benoît V <benoit@vandanjon.fr>
2022-07-18 09:18:27 +0200

    simplification for examples

--- backend/routes.js
+++ backend/routes.js
@@ -1,8 +1,6 @@
-const baseRouter = require("./controllers/base");
 const usersRouter = require("./controllers/users");
 
 const setup = (app) => {
-	app.use("/", baseRouter);
 	app.use("/users", usersRouter);
 };
 
