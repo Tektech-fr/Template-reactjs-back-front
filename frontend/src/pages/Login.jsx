3112e77c1f2d91f99156af9ca4dc1f0e35275b77
Benoît V <benoit@vandanjon.fr>
2022-07-23 18:00:45 +0200

    basic form case

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,9 +1,36 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Login = () => {
+	const [username, setUsername] = useState();
+	const [password, setPassword] = useState();
+
+	const handleNameChange = (e) => {
+		setUsername(e.target.value);
+	};
+
+	const handlePassChange = (e) => {
+		setPassword(e.target.value);
+	};
+
 	return (
 		<main id="MainLogin">
 			<h1>Login</h1>
+
+			<form>
+				<label>
+					Firstname :{" "}
+					<input type="text" value={username} onChange={handleNameChange} />
+				</label>
+
+				<label>
+					Password :{" "}
+					<input type="password" value={password} onChange={handlePassChange} />
+				</label>
+			</form>
+
+			<h2>{username}</h2>
+			<h2>{password}</h2>
 
 			<Link to="/admin">
 				<button>CLICK ME</button>
