65864a19d422ef7670aa9d0361da4043a79b6fe2
Benoît V <benoit@vandanjon.fr>
2022-07-24 16:36:58 +0200

    working fake call auth

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
 
 import Home from "@page/Home";
 import Login from "@page/Login";
@@ -7,17 +8,45 @@ import NotFound from "@page/NotFound";
 import About from "@page/About";
 
 import NavBar from "@comp/NavBar";
+import ProtectedRoute from "@comp/ProtectedRoute";
 
 const App = () => {
+	const [isLoggedIn, setIsLoggedIn] = useState(null);
+
+	const logIn = () => {
+		setIsLoggedIn(true);
+	};
+
+	const logOut = () => {
+		setIsLoggedIn(false);
+	};
+
 	return (
 		<BrowserRouter>
 			<NavBar />
 
+			{isLoggedIn ? (
+				<button onClick={logOut}>Logout</button>
+			) : (
+				<button onClick={logIn}>Login</button>
+			)}
+
 			<Routes>
 				<Route path="/" element={<Home />} />
+
 				<Route path="/about" element={<About />} />
+
 				<Route path="/login" element={<Login />} />
-				<Route path="/admin" element={<Admin />} />
+
+				<Route
+					path="/admin"
+					element={
+						<ProtectedRoute isLoggedIn={isLoggedIn}>
+							<Admin />
+						</ProtectedRoute>
+					}
+				/>
+
 				<Route path="*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,11 @@
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ isLoggedIn, children }) => {
+	if (!isLoggedIn) {
+		return <Navigate to="/" replace />;
+	}
+
+	return children;
+};
+
+export default ProtectedRoute;
